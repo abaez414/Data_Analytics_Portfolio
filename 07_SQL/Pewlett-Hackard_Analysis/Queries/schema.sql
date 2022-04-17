e9508ce49c18fe4198365eaff3bfb1ba04e51d29
abaez414 <abaez414@gmail.com>
2022-04-16 22:51:04 -0400

    7.3.1 update

--- 07_SQL/Pewlett-Hackard_Analysis/Queries/schema.sql
+++ 07_SQL/Pewlett-Hackard_Analysis/Queries/schema.sql
@@ -8,7 +8,7 @@ CREATE TABLE departments (
 
 CREATE TABLE employees (
 	emp_no INT NOT NULL,
-	birth_date VARCHAR NOT NULL,
+	birth_date DATE NOT NULL,
 	first_name VARCHAR NOT NULL,
 	last_name VARCHAR NOT NULL,
 	gender VARCHAR NOT NULL,
@@ -51,7 +51,7 @@ CREATE TABLE titles (
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-PRIMARY KEY (emp_no)
+PRIMARY KEY (emp_no, title, from_date)
 );
 
 SELECT * FROM departments;
