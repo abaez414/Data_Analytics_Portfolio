63c43e202f3ccd959153e9c41c9bd85f7e7d7fb6
abaez414 <abaez414@gmail.com>
2022-07-08 19:37:39 -0400

    another folder update

--- 13_GeoJSON/Mapping_Earthquakes/Mapping_Lines/static/js/logic.js
+++ 13_GeoJSON/Mapping_Earthquakes/Mapping_Lines/static/js/logic.js
@@ -2,20 +2,19 @@
 console.log("working");
 
 // Create the map object with a center zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+let map = L.map('mapid').setView([36.1733, -120.1794], 7);
 
-// Get data from cities.js
-let cityData = cities;
+// Coordinates for each point to be used in the polyline
+let line = [
+    [33.9416, -118.4085],
+    [37.6213, -122.3790]
+];
 
-// Loop through the cities array and create one marker for each city.
-cityData.forEach(function(city) {
-    console.log(city)
-    L.circleMarker(city.location, {
-        radius: city.population/100000
-    })
-    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-  .addTo(map);
-});
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+    color: "red"
+  }).addTo(map);
+
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
