6e17895d1e238921fa026d8275bd32880b281fc8
abaez414 <abaez414@gmail.com>
2022-08-02 21:26:33 -0400

    module 15 update

--- 15_R_Analysis/MechaCar_Statistical_Analysis/MechaCarChallenge.R
+++ 15_R_Analysis/MechaCar_Statistical_Analysis/MechaCarChallenge.R
@@ -11,3 +11,29 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 
 #6. Determine the p-value and r-squared value
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=Mecha_mpg_table))
+
+# Deliverable 2
+
+#2. Import and read Suspension_coil file
+mecha_coil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F) 
+
+#3. Create a total summary dataframe
+total_summary <- mecha_coil %>% summarize(Mean_PSI=mean(PSI), Median_PSI=median(PSI), Var_PSI=var(PSI), Std_Dev_PSI=sd(PSI), Num_Coil=n(), .groups = 'keep')
+
+#4. Create a lot summary
+lot_summary <- mecha_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
+
+
+#Deliverable 3
+#1. Perform t.test()
+t.test(mecha_coil$PSI, mu=1500)
+
+#2. Perform t.test 3 more times for each lot
+lot1 <- subset(mecha_coil, Manufacturing_Lot=="Lot1")
+lot2 <- subset(mecha_coil, Manufacturing_Lot=="Lot2")
+lot3 <- subset(mecha_coil, Manufacturing_Lot=="Lot3")
+
+t.test(lot1$PSI,mu=1500)
+t.test(lot2$PSI,mu=1500)
+t.test(lot3$PSI,mu=1500)
+
